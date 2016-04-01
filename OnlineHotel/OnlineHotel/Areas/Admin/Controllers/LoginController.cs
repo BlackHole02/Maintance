ff82f2340bc22d74b5cf9e4b708c46dccea38430
BlackHole02 <hangnguyen081995@gmail.com>
2016-04-01 15:05:09 +0700

    DATABASE + PROJECT

---
+++ OnlineHotel/OnlineHotel/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,48 @@
+﻿using Model.DAO;
+using OnlineHotel.Areas.Admin.Models;
+using OnlineHotel.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineHotel.Areas.Admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Admin/Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var dao = new ACCOUNT_DAO();
+                var result = dao.Login(model.UserName, model.Password);
+                if (result == 1)
+                {
+                    var account = dao.GetById(model.UserName);
+                    var accountSession = new AccountLogin();
+                    accountSession.UserName = account.UserName;
+                    accountSession.AccountID = account.ID;
+
+                    Session.Add(CommonConstants.ACCOUNT_SESSION, accountSession);
+                    return RedirectToAction("Index", "Home");
+                }
+                else if (result == 0)
+                {
+                    ModelState.AddModelError("", "Tài khoản không tồn tại.");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Đăng nhập không đúng");
+                }
+            }
+            return View("Index");
+        }
+    }
+}
---
+++ OnlineHotel/OnlineHotel/Common/AccountLogin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineHotel.Common
+{
+    [Serializable]
+    public class AccountLogin
+    {
+        public int AccountID { set; get; }
+        public string UserName { set; get; }
+    }
+}
